a8775001f3cc7c0a0e654195a63a2f6f832aa8d1
anthonyrohloff <anthony.rohloff@gmail.com>
2024-09-23 21:10:16 -0400

    Improved works cited on entry1

--- pages/blog_entries/entry1.py
+++ pages/blog_entries/entry1.py
@@ -2,7 +2,7 @@ import dash
 import dash_core_components as dcc
 from dash import html
 
-dash.register_page(__name__, path="/blog/entry1")
+dash.register_page(__name__, path="/blog/entry1", title="The Impending Impact of Quantum Computing on Encryption Algorithms")
 
 post_content = """
 # The Impending Impact of Quantum Computing on Encryption Algorithms
@@ -11,8 +11,6 @@ post_content = """
 University of Cincinnati  
 College of Engineering and Applied Science  
 Bachelor of Science in Cybersecurity Engineering
-
-## The Impending Impact of Quantum Computing on Encryption Algorithms
 
 Our fastest supercomputers take thousands of years to solve certain complex and challenging problems. Quantum computers can solve these problems in minutes. Quantum computing ranging from hardware to algorithms is on the bleeding edge of computation. This is achieved by utilizing quantum mechanics to operate using ‘qubits’ as opposed to binary bits that traditional computational machines use. Even our most advanced encryption algorithms are susceptible to quantum computing. As this new technology becomes more accessible major changes will need to be made in the way information is encrypted and distributed.
 
@@ -25,13 +23,21 @@ Fortunately there are solutions being developed to prepare for this inevitabilit
 Quantum computing and its ability to use qubits to crack the current standard encryption algorithms is a looming threat to the security of confidential information. The research and development of ever-stronger quantum computers shows the ability of these machines to perform calculations at unprecedented speeds by utilizing the superposition of elementary particles to create bits that can hold values other than 0 and 1. This issue has been extensively investigated and resulted in an output of standard algorithms that can stump the strongest computers of each type classical and quantum. Much work remains in implementing these protections as quantum computing becomes more accessible but there is time to achieve the necessary security level before the critical point emerges.
 
 ### Works Cited
-- Computer Security Division Information Technology Laboratory. “Post-Quantum Cryptography Standardization - Post-Quantum Cryptography: CSRC.” CSRC 22 Nov. 2023 csrc.nist.gov/projects/post-quantum-cryptography/post-quantum-cryptography-standardization. 
-- Giles Martin. “Explainer: What Is a Quantum Computer?” MIT Technology Review MIT Technology Review 20 Oct. 2021 www.technologyreview.com/2019/01/29/66141/what-is-quantum-computing/. 
-- Giles Martin. “Explainer: What Is Post-Quantum Cryptography?” MIT Technology Review MIT Technology Review 5 Apr. 2024 www.technologyreview.com/2019/07/12/134211/explainer-what-is-post-quantum-cryptography/. 
-- “NIST Announces First Four Quantum-Resistant Cryptographic Algorithms.” NIST 7 July 2022 www.nist.gov/news-events/news/2022/07/nist-announces-first-four-quantum-resistant-cryptographic-algorithms. 
-- Schneider Josh and Ian Smalley. “What Is a Qubit?” IBM 28 Feb. 2024 www.ibm.com/topics/qubit#:~:text=A%20qubit%2C%20or%20quantum%20bit%2C%20is%20the%20basicby%20classical%20computers%20to%20encode%20information%20in%20binary. 
-- Schneider Josh and Ian Smalley. “What Is Quantum Computing?” IBM 8 Sept. 2021 www.ibm.com/topics/quantum-computing#:~:text=Quantum%20computing%20uses%20specialized%20technology%E2%80%94including%20computer%20hardware%20andsupercomputers%20can%E2%80%99t%20solve%2C%20or%20can%E2%80%99t%20solve%20quickly%20enough. 
-- “What Is a Qubit?” Institute for Quantum Computing University of Waterloo 5 Oct. 2023 uwaterloo.ca/institute-for-quantum-computing/quantum-101/quantum-information-science-and-technology/what-qubit.
+
+Computer Security Division Information Technology Laboratory. “Post-Quantum Cryptography Standardization - Post-Quantum Cryptography: CSRC.” *CSRC*, 22 Nov. 2023, [csrc.nist.gov/projects/post-quantum-cryptography/post-quantum-cryptography-standardization](https://csrc.nist.gov/projects/post-quantum-cryptography/post-quantum-cryptography-standardization).
+
+Giles Martin. “Explainer: What Is a Quantum Computer?” *MIT Technology Review*, 20 Oct. 2021, [www.technologyreview.com/2019/01/29/66141/what-is-quantum-computing/](https://www.technologyreview.com/2019/01/29/66141/what-is-quantum-computing/).
+
+Giles Martin. “Explainer: What Is Post-Quantum Cryptography?” *MIT Technology Review*, 5 Apr. 2024, [www.technologyreview.com/2019/07/12/134211/explainer-what-is-post-quantum-cryptography/](https://www.technologyreview.com/2019/07/12/134211/explainer-what-is-post-quantum-cryptography/).
+
+“NIST Announces First Four Quantum-Resistant Cryptographic Algorithms.” *NIST*, 7 July 2022, [www.nist.gov/news-events/news/2022/07/nist-announces-first-four-quantum-resistant-cryptographic-algorithms](https://www.nist.gov/news-events/news/2022/07/nist-announces-first-four-quantum-resistant-cryptographic-algorithms).
+
+Schneider, Josh, and Ian Smalley. “What Is a Qubit?” *IBM*, 28 Feb. 2024, [www.ibm.com/topics/qubit#:~:text=A%20qubit%2C%20or%20quantum%20bit%2C%20is%20the%20basicby%20classical%20computers%20to%20encode%20information%20in%20binary](https://www.ibm.com/topics/qubit#:~:text=A%20qubit%2C%20or%20quantum%20bit%2C%20is%20the%20basicby%20classical%20computers%20to%20encode%20information%20in%20binary).
+
+Schneider, Josh, and Ian Smalley. “What Is Quantum Computing?” *IBM*, 8 Sept. 2021, [www.ibm.com/topics/quantum-computing#:~:text=Quantum%20computing%20uses%20specialized%20technology%E2%80%94including%20computer%20hardware%20andsupercomputers%20can%E2%80%99t%20solve%2C%20or%20can%E2%80%99t%20solve%20quickly%20enough](https://www.ibm.com/topics/quantum-computing#:~:text=Quantum%20computing%20uses%20specialized%20technology%E2%80%94including%20computer%20hardware%20andsupercomputers%20can%E2%80%99t%20solve%2C%20or%20can%E2%80%99t%20solve%20quickly%20enough).
+
+“What Is a Qubit?” *Institute for Quantum Computing*, University of Waterloo, 5 Oct. 2023, [uwaterloo.ca/institute-for-quantum-computing/quantum-101/quantum-information-science-and-technology/what-qubit](https://uwaterloo.ca/institute-for-quantum-computing/quantum-101/quantum-information-science-and-technology/what-qubit).
+
 """
 
 layout = html.Div([
